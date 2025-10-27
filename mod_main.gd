128c2945662c1ae9fcbeeacc0fcd6bec709923f5
eltoro0815 <th_ru@web.de>
2025-10-27 21:26:08 +0100

    only load gui if mod options is available

--- mod_main.gd
+++ mod_main.gd
@@ -4,6 +4,7 @@ const MOD_DIR = "Eltoro0815-BetterChanceForLegendaryWeapons/"
 const LOG_NAME = "Eltoro0815-BetterChanceForLegendaryWeapons"
 const MOD_NAME = "Eltoro0815-BetterChanceForLegendaryWeapons"
 const CONFIG_NAME = "bclw_config"
+const MOD_OPTIONS_MOD_ID = "dami-ModOptions"
 
 var dir = ""
 var ext_dir = ""
@@ -25,11 +26,15 @@ func _init(_modLoader = ModLoader):
 	
 	ModLoaderMod.install_script_extension(ext_dir + "singletons/item_service.gd")
 	
-	# Install menu extension for mod options
-	ModLoaderMod.install_script_extension(ext_dir + "menus/pages/menu_choose_options.gd")
 
 func _ready(_modLoader = ModLoader):
 	ModLoaderLog.info("Done", LOG_NAME)
+
+	if ModLoaderMod.is_mod_loaded(MOD_OPTIONS_MOD_ID):
+		# Install menu extension for mod options
+		ModLoaderMod.install_script_extension(ext_dir + "menus/pages/menu_choose_options.gd")
+	else:
+		ModLoaderLog.info("Optionaler Mod \"%s\" nicht gefunden. Überspringe Menüerweiterung." % MOD_OPTIONS_MOD_ID, LOG_NAME)
 	
 	# Create and start the config manager
 	var config_manager = load("res://mods-unpacked/Eltoro0815-BetterChanceForLegendaryWeapons/bclw_config_manager.gd").new()
